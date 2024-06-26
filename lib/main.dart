b8ef83436ba420dc2b09fc9fe16b1dc06a7776db
Sumantxkumar <sumantchampion@gmail.com>
2024-06-26 21:02:54 +0530

    https://github.com/londonappbrewery/Flutter-Course-Resources.git

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'oto.dart';
+import 'wrap_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const OtpScreen());
+        home: const WrapScreen());
   }
 }
 
